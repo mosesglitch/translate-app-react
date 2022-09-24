98edfd1271c78f115e8a82a968f3c7d4fc213333
mosesglitch <mosesgachuru15@gmail.com>
2022-09-24 16:15:11 +0300

    modify app.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React, { useState } from "react";
 import Field from "./components/field";
 import Translate from "./components/translate";
 import Languages from "./components/languages";
-
+import "./styles.css";
 export default function App() {
+  const [language, setLanguage] = useState("es");
+  const [text, setText] = useState("");
+
   return (
     <div>
-      <h3>Hi there!</h3>
+      <Field onChange={setText} />
+      <Languages language={language} onLanguageChange={setLanguage} />
+      <hr />
+      <Translate text={text} language={language} />
     </div>
   );
 }
